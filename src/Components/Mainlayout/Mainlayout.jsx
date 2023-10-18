6659ee7dad0b2472a35a618976041b263b564b99
Shamimreza82 <shamimrezaone@gmail.com>
2023-10-18 21:56:57 +0600

    added dark and light mode theme

--- src/Components/Mainlayout/Mainlayout.jsx
+++ src/Components/Mainlayout/Mainlayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navber from './Navber';
-import Footer from './Footer';
+import Navber2 from './Navber2';
 
 const Mainlayout = () => {
     return (
         <div>
+            <Navber2></Navber2>
             <Navber></Navber>
             <Outlet></Outlet>
         </div>
---
+++ src/Components/Mainlayout/Navber2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navber2 = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Navber2;
